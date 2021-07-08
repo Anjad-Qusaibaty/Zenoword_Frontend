1a3c65987217c405f7633547a2b6473270846ba1
Anjad-Qusaibaty <anjad.qusaibaty@gmail.com>
2021-07-09 01:30:15 +0200

    testing constants

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,4 +2,8 @@ const { REACT_APP_API_URL } = process.env;
 
 export const apiUrl = REACT_APP_API_URL || "http://localhost:4000";
 
+// export const apiUrl = REACT_APP_API_URL;
+
+// export const apiUrl = "http://localhost:4000";
+
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
