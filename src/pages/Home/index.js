3f53cb9fd30bb5fa53b818936ec36788275a8988
Anjad-Qusaibaty <anjad.qusaibaty@gmail.com>
2021-07-09 12:45:37 +0200

    added carousal for home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "./index.css";
+import Carousel from "react-bootstrap/Carousel";
+import "../My_library/library_page_view.css";
+import Onlinearticle from "../My_library/Online_Article.png";
 
 export default function Home() {
   return (
@@ -48,10 +51,10 @@ export default function Home() {
             color: "#1C8ABE",
           }}
         >
-          How it works
+          A Peek Inside
         </p>
       </div>
-      <div className="containterIfram">
+      {/* <div className="containterIfram">
         <iframe
           className="responsive-iframe"
           // width="560"
@@ -62,7 +65,138 @@ export default function Home() {
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
         ></iframe>
-      </div>
+      </div> */}
+      {/* 
+style={{ border: "solid 1px black", backgroundColor: "red" }} */}
+
+      <Carousel
+        style={{
+          marginBottom: "5rem",
+        }}
+      >
+        <Carousel.Item interval={1000}>
+          <div className="allpage">
+            <div className="container1">
+              <div className="image">
+                <img
+                  src="http://books.google.com/books/content?id=u8w_DwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api"
+                  alt="Extract 1"
+                />
+              </div>
+              <div className="text">
+                <div className="mainExtract">
+                  <p>
+                    Our minds are built on the hunting-and-gathering platforms
+                    of our bodies.
+                  </p>
+                </div>
+                <div className="extractInfo">
+                  <p>
+                    <span className="spans">Media type:</span>Book
+                  </p>
+                  <p>
+                    <span className="spans">Title:</span> 12 Rules for life (An
+                    Antidote to Chaos)
+                  </p>
+                  <p>
+                    <span className="spans">Author:</span> Jordan B. Peterson
+                  </p>
+
+                  <p>
+                    <span className="spans">Page:</span> 92
+                  </p>
+                </div>
+                <div className="tagscont">
+                  <div className="extractTag"># Hunters & Gatherers</div>
+                  <div className="extractTag"># Mind & Body</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item interval={500}>
+          <div className="allpage">
+            <div className="container1">
+              <div className="image">
+                <img
+                  src="https://books.google.nl/books/content?id=SEsIPQAACAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73hSAP6agfOZHMujth1zZwLuCWd0TgETmvbPIEf_kx7leAh5qry8pGWsthgTsL4rV3R-RA6jR0wZVdVqhnSyGX7P-X8POzmGuf9232q8caqCAbuKXpTZxcdFNRs8Cm8zNl6u-EY"
+                  alt="Extract 2"
+                />
+              </div>
+              <div className="text">
+                <div className="mainExtract">
+                  <p>
+                    ...it did not really matter what we expected from life, but
+                    rather what life expected from us.
+                  </p>
+                </div>
+                <div className="extractInfo">
+                  <p>
+                    <span className="spans">Media type:</span>Book
+                  </p>
+                  <p>
+                    <span className="spans">Title:</span> Man's Search For
+                    Meaning
+                  </p>
+                  <p>
+                    <span className="spans">Author:</span> Viktor E. Frankl
+                  </p>
+
+                  <p>
+                    <span className="spans">Page:</span> 85
+                  </p>
+                </div>
+                <div className="tagscont">
+                  <div className="extractTag"># Attitude</div>
+                  <div className="extractTag"># Life</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Carousel.Item>
+        <Carousel.Item>
+          <div className="allpage">
+            <div className="container1">
+              <div className="image">
+                <img src={Onlinearticle} alt="Extract 3" />
+              </div>
+              <div className="text">
+                <div className="mainExtract">
+                  <p>
+                    Food is a persistent nuanced balance between short term
+                    enjoyment & long term reward.
+                  </p>
+                </div>
+                <div className="extractInfo">
+                  <p>
+                    <span className="spans">Media type:</span>Online Article
+                  </p>
+                  <p>
+                    <span className="spans">Title:</span>Low-carb thinking for
+                    regular people
+                  </p>
+                  <p>
+                    <span className="spans">Author:</span> Philip Marais
+                  </p>
+
+                  <p>
+                    <a
+                      href="https://medium.com/@finmn/low-carb-thinking-for-regular-people-c82354266beb"
+                      target="blank"
+                    >
+                      <span className="spans">Link</span>
+                    </a>
+                  </p>
+                </div>
+                <div className="tagscont">
+                  <div className="extractTag"># Keto Diet</div>
+                  <div className="extractTag"># Food</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 }
